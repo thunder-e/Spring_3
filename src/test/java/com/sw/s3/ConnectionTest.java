713bc356e12e5a404bd3246d1690e932b1d11a8a
thunder-e <79903353+thunder-e@users.noreply.github.com>
2021-03-22 17:32:48 +0900

    commit

---
+++ src/test/java/com/sw/s3/ConnectionTest.java
@@ -0,0 +1,28 @@
+package com.sw.s3;
+
+import static org.junit.Assert.*;
+
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+public class ConnectionTest extends MyAbstractTest { //Annotation 같이 상속받아옴
+ 
+	@Autowired
+	private DataSource dataSource;
+	
+	@Autowired
+	private SqlSession sqlSession;
+	
+	
+	@Test
+	public void connectionTest() throws Exception {
+		assertNotNull(sqlSession);
+	}
+	
+	
+
+}
