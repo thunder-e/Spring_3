9e63d90063f7c16d42b7c3243a4ae1cb97bcc8c9
thunder-e <79903353+thunder-e@users.noreply.github.com>
2021-03-23 14:02:44 +0900

    bankbook delete

--- src/main/java/com/sw/s3/bankbook/BankBookDAO.java
+++ src/main/java/com/sw/s3/bankbook/BankBookDAO.java
@@ -16,40 +16,38 @@ public class BankBookDAO {
 	
 	@Autowired
 	private SqlSession sqlSession; //root-context.xml에 있는
-	private final String NAMESPACE="com.sw.s3.bankbook.BankBookDAO";
+	private final String NAMESPACE="com.sw.s3.bankbook.BankBookDAO"; //Mapper의 namespace이름과 동일하게
+	
+	
+	public int setUpdate(BankBookDTO bankBookDTO) throws Exception {
+		return sqlSession.update(NAMESPACE + ".setUpdate", bankBookDTO);
+	}
+	
+	
+	public int setDelete(BankBookDTO bankBookDTO) throws Exception {
+		return sqlSession.delete(NAMESPACE + ".setDelete", bankBookDTO);
+	}
+	
 
 	public int setWrite(BankBookDTO bankBookDTO) throws Exception {
 		int result = sqlSession.insert(NAMESPACE+".setWrite", bankBookDTO); //파라미터는 타입 하나만 보낼 수 있어
 		return result;
-
 	}
 
 
-
-
-
 	public BankBookDTO getSelect(BankBookDTO bankBookDTO)throws Exception{
-		
-
-
-	
+//		long num = 1L;
+		bankBookDTO = sqlSession.selectOne(NAMESPACE+".getSelect", bankBookDTO); //없으면 자동으로 null이 들어가
 		return bankBookDTO;
-
 	}	
-
-
-
-
 
 	//getList
 	//bankbook table의 모든 데이터 조회 후 리턴
 	public List<BankBookDTO> getList() throws Exception {
-		ArrayList<BankBookDTO> ar = new ArrayList<BankBookDTO>();
-
-
-	
-		return ar;
+		return sqlSession.selectList(NAMESPACE+".getList");
 	}
+	
+	
 
 
 
--- src/test/java/com/sw/s3/bankbook/BankBookDAOTest.java
+++ src/test/java/com/sw/s3/bankbook/BankBookDAOTest.java
@@ -2,6 +2,8 @@ package com.sw.s3.bankbook;
 
 import static org.junit.Assert.*;
 
+import java.util.List;
+
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -14,7 +16,50 @@ public class BankBookDAOTest extends MyAbstractTest{
 	private BankBookDAO bankBookDAO;
 	
 	@Test
-	public void setWrite() throws Exception{
+	public void setUpdateTest() throws Exception {
+		BankBookDTO bankBookDTO = new BankBookDTO();
+		bankBookDTO.setBookNumber(5);
+		
+		bankBookDTO = bankBookDAO.getSelect(bankBookDTO);
+		
+		bankBookDTO.setBookName("New Product");
+		
+
+		int result = bankBookDAO.setUpdate(bankBookDTO);
+		assertEquals(1, result);
+	}
+	
+	
+	
+	
+	//@Test
+	public void setDeleteTest() throws Exception {
+		BankBookDTO bankBookDTO = new BankBookDTO(); //파라미터로 보낼거니까 만들어놔야
+		bankBookDTO.setBookNumber(8);
+		int result = bankBookDAO.setDelete(bankBookDTO);
+		
+		assertEquals(1, result);
+	}
+	
+	
+	
+	@Test
+	public void getListTest() throws Exception {
+		List<BankBookDTO> ar = bankBookDAO.getList();
+		assertNotEquals(0, ar.size());
+	}
+	
+	
+	//@Test
+	public void getSelectTest() throws Exception{
+		BankBookDTO bankBookDTO = bankBookDAO.getSelect(null);
+		assertNotNull(bankBookDTO);
+	}
+	
+	
+	
+	//@Test
+	public void setWriteTest() throws Exception{
 		BankBookDTO bankBookDTO = new BankBookDTO();
 		bankBookDTO.setBookName("Test");
 		bankBookDTO.setBookRate(0.12);
