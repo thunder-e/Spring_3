6a6687c3790156166f28ed1f82c078610a8a12dc
thunder-e <79903353+thunder-e@users.noreply.github.com>
2021-03-23 14:19:53 +0900

    .

---
+++ src/test/java/com/sw/s3/member/MemberDAOTest.java
@@ -0,0 +1,15 @@
+package com.sw.s3.member;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MemberDAOTest {
+
+
+
+	public int memberJoin() throws Exception {
+		
+	}
+
+}
