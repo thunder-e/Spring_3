67cd427632aa7657c56ebfc1536ebfd8a4dd5107
thunder-e <pinkpear0114@naver.com>
2021-03-24 18:26:18 +0900

    member session

--- src/main/java/com/sw/s3/member/MemberController.java
+++ src/main/java/com/sw/s3/member/MemberController.java
@@ -1,11 +1,11 @@
 package com.sw.s3.member;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/member/**")
@@ -21,11 +21,11 @@ public class MemberController {
 	}
 	
 	@RequestMapping(value="memberLogin", method=RequestMethod.POST) 	// DB
-	public String memberLogin(MemberDTO memberDTO) throws Exception {
-	
+	public String memberLogin(MemberDTO memberDTO, HttpSession session) throws Exception {
 		memberDTO = memberService.memberLogin(memberDTO);	 // 받아온 dto를 service로 보내
+		System.out.println(memberDTO);
+		session.setAttribute("member", memberDTO);
 		return "redirect:../";
-
 	}
 	
 	
--- src/main/webapp/WEB-INF/views/home.jsp
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -1,5 +1,4 @@
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
-<%@ page session="false" %>
 <html>
 <head>
 	<title>Home</title>
@@ -12,8 +11,16 @@
 <P>  The time on the server is ${serverTime}. </P>
 
 <a href="./bankbook/bankbookList">BankBook List</a>
+
+<c:if test="${empty member}">
 <a href="./member/memberJoin">Join</a>
 <a href="./member/memberLogin">Login</a>
+</c:if>
+
+<c:if test="${not empty member}">
+<a href="./member/memberPage">MyPage</a>
+<a href="./member/memberLogout">Logout</a>
+</c:if>
 
 </body>
 </html>
